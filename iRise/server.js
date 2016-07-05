138a7f9929e1c4c0967b68fc365204b2c8491729
SJairam <SJairam@UPLIFT-BTDGY52>
2016-07-05 13:49:28 -0500

    Add structure
    - Used express since its what I have been doing
    - assumed we could change tableau iframes with css
    - tests with mocha and assert cause probably not doing too much crazy async stuff
    - 'require(config.json)' to get config information in the backend

--- iRise/server.js
+++ iRise/server.js
@@ -1,6 +1,13 @@
-﻿var http = require('http');
+﻿var exp = require('express');
+var site = exp();
+var server = require('http').createServer(site);
+var io = require('socket.io')(server);
+var fs = require('fs');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+site.use("/", exp.static(__dirname + '/assets'));
+server.listen(port);
+
+site.get('/', function (req, res) {
+  res.sendfile(__dirname + '/assets/index.html');
+});
